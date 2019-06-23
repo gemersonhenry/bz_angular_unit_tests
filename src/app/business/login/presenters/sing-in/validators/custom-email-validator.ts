4b04474443c9a3f3e98787374705364f1e1ee95b
BZGemerson <henryingtel@gmail.com>
2019-06-23 15:11:39 -0500

    feat(test): corrigiendo error de implementación de validator test

--- src/app/business/login/presenters/sing-in/validators/custom-email-validator.ts
+++ src/app/business/login/presenters/sing-in/validators/custom-email-validator.ts
@@ -4,7 +4,7 @@ import { EMAIL_REG_EXP } from '../../../constants/validator-constants';
 export function customEmailValidator(): ValidatorFn {
   return (control: AbstractControl) => {
     const { value } = control;
-    const objReturn = { customEmail: true };
+    const objReturn = { customEmailValidator: true };
     return EMAIL_REG_EXP.test(value) ? null : objReturn;
   };
 }
